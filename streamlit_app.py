07445d2a935afe1ac8a0ffa4e1cce9ab6c138a6d
rjgriggs <39922220+rjgriggs@users.noreply.github.com>
2022-12-13 00:44:07 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,7 @@ from urllib.error import URLError
 
 
 streamlit.title("My Mom's New Healthy Diner")
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
